14a0bacbca64eff5ccb27b1c5aad3540622928b1
bogzak <bogdan.zaakh@gmail.com>
2023-04-16 02:25:11 +0300

    refactor main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import streamlit as st
 from indexing import IndexingGoogle
 
+
 def main():
-    st.title("Google Indexing")
+    st.title('Google Indexing')
 
     # JSON credentials file upload
-    credentials_file = st.file_uploader("Upload JSON credentials file")
+    credentials_file = st.file_uploader('Upload JSON credentials file')
 
     # URL list input
-    url_list = st.text_area("Enter URLs to be indexed, separated by a new line")
+    url_list = st.text_area('Enter URLs to be indexed, separated by a new line')
 
     # Send URLs button
-    if st.button("Send URLs for indexing"):
-        urls = url_list.strip().split("\n") if url_list.strip() else []
+    if st.button('Send URLs for indexing'):
+        urls = url_list.strip().split('\n') if url_list.strip() else []
         indexing_google = IndexingGoogle()
 
         if credentials_file:
@@ -22,10 +23,10 @@ def main():
                 result = indexing_google.send_urls(urls)
                 st.write(result)
             else:
-                st.write("Error: Credentials file is empty")
+                st.write('Error: Credentials file is empty')
         else:
-            st.write("Error: No credentials file uploaded")
+            st.write('Error: No credentials file uploaded')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
