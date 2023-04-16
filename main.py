25201701f1425e6498f34aed33835f5fa282cf09
bogzak <bogdan.zaakh@gmail.com>
2023-04-16 03:01:35 +0300

    refactor options to choice the type of request

--- main.py
+++ main.py
@@ -9,10 +9,19 @@ def main():
     credentials_file = st.file_uploader('Upload JSON credentials file')
 
     # URL list input
-    url_list = st.text_area('Enter URLs to be indexed, separated by a new line')
+    url_list = st.text_area('Enter URLs, separated by a new line')
+
+    type_mapping = {
+        'Update': 'URL_UPDATED',
+        'Delete': 'URL_DELETED'
+    }
+
+    # Get the user's selection from the app
+    user_selection = st.selectbox('Select action', ['Update', 'Delete'])
+    option = type_mapping[user_selection]
 
     # Define a radio button to allow user to choose between "URL_UPDATED" and "URL_DELETED"
-    option = st.radio('Choose an option:', ('URL_UPDATED', 'URL_DELETED'))
+    # option = st.radio('Choose an option:', ('URL_UPDATED', 'URL_DELETED'))
 
     # Send URLs button
     if st.button('Send URLs'):
